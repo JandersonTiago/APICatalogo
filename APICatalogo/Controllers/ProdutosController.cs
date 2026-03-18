1d8649c5659de826e223c61e41efbf53729d4414
Janderson Silva <janderson.tsilva@outlook.com>
2026-03-17 23:09:16 -0300

    ProdutosController - GET

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,4 +1,5 @@
 ﻿using APICatalogo.Context;
+using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace APICatalogo.Controllers;
@@ -12,5 +13,17 @@ public class ProdutosController : ControllerBase
     public ProdutosController(AppDbContext context)
     {
         _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.ToList();
+
+        if (produtos is null) 
+        {
+            return NotFound("Produtos não encontrados...");
+        }
+        return produtos;
     }
 }
