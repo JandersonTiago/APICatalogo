7f3bcaf75e2e97116835d62e6e83c7dad8010651
Janderson Silva <janderson.tsilva@outlook.com>
2026-05-19 22:30:36 -0300

    Validacao Personalizada II

--- APICatalogo/Models/Produto.cs
+++ APICatalogo/Models/Produto.cs
@@ -6,14 +6,14 @@ using System.Text.Json.Serialization;
 namespace APICatalogo.Models;
 
 [Table("Produtos")]
-public class Produto
+public class Produto : IValidatableObject
 {
     [Key]
     public int ProdutoId { get; set; }
 
     [Required(ErrorMessage = "O nome do produto é obrigatório.")]
     [StringLength(80, ErrorMessage = "O nome do produto deve ter no máximo {1} e no mínimo {2} caracteres.", MinimumLength = 5)]
-    [PrimeiraLetraMaiuscula]
+    //[PrimeiraLetraMaiuscula]
     public string? Nome { get; set; }
 
     [Required]
@@ -36,4 +36,29 @@ public class Produto
 
     [JsonIgnore]
     public Categoria? Categoria { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new
+                    ValidationResult("A primeira letra do nome do produto deve ser maiúscula.",
+                        new[]
+                        { nameof(this.Nome) }
+                        );
+            }
+
+            if (this.Estoque <= 0)
+            {
+                yield return new
+                    ValidationResult("O estoque deve ser maior que zero.",
+                        new[]
+                        { nameof(this.Estoque) }
+                        );
+            }
+        }
+    }
 }
