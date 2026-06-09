4937a13fc7feee2ee6c93996fcc23dd2f49308bc
Janderson Silva <janderson.tsilva@outlook.com>
2026-06-08 22:08:03 -0300

    Criando um log customizado

---
+++ APICatalogo/Logging/CustomLoggerProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+
+namespace APICatalogo.Logging;
+
+public class CustomLoggerProvider : ILoggerProvider
+{
+    readonly CustomLoggerProviderConfiguration loggerconfig;
+    readonly ConcurrentDictionary<string, CustomerLogger> loggers =
+        new ConcurrentDictionary<string, CustomerLogger>();
+
+    public CustomLoggerProvider(CustomLoggerProviderConfiguration config)
+    {
+        loggerconfig = config;
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return loggers.GetOrAdd(categoryName, name => new CustomerLogger(name, loggerconfig));
+    }
+
+    public void Dispose()
+    {
+        loggers.Clear();
+    }
+}
---
+++ APICatalogo/Logging/CustomerLogger.cs
@@ -0,0 +1,48 @@
+﻿
+namespace APICatalogo.Logging;
+
+public class CustomerLogger : ILogger
+{
+    readonly string loggerName;
+    readonly CustomLoggerProviderConfiguration loggerconfig;
+
+    public CustomerLogger(string name, CustomLoggerProviderConfiguration config)
+    {
+        loggerName = name;
+        loggerconfig = config;
+    }
+
+    public IDisposable BeginScope<TState>(TState state)
+    {
+        return null;
+    }
+
+    public bool IsEnabled(LogLevel logLevel)
+    {
+        return logLevel == loggerconfig.LogLevel;
+    }
+
+    public void Log<TState>(LogLevel logLevel, EventId eventId, TState state,
+                            Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        string menssagem = $"{logLevel.ToString()}: {eventId.Id} - {formatter(state, exception)}";
+        EscreverTextoNoArquivo(menssagem);
+    }
+
+    private void EscreverTextoNoArquivo(string menssagem)
+    {
+        string caminhoArquivoLog = @"D:\Backup\Cursos\CursoWebAPI\APICatalogo\Dados\Log\Janderson_log.txt";
+        using (StreamWriter streamWriter = new StreamWriter(caminhoArquivoLog, true))
+        {
+            try
+            {
+                streamWriter.WriteLine(menssagem);
+                streamWriter.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
