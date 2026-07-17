b06b42b80605e20b7f695f69cbbaa32909db31cf
Janderson Silva <janderson.tsilva@outlook.com>
2026-07-16 22:42:20 -0300

    Fazendo o mapeamento usando o AutoMapper

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,4 +1,5 @@
 using APICatalogo.Context;
+using APICatalogo.DTOs.Mappings;
 using APICatalogo.Extensions;
 using APICatalogo.Filters;
 using APICatalogo.Logging;
@@ -37,6 +38,8 @@ builder.Logging.AddProvider(new CustomLoggerProvider(
     {
         LogLevel = LogLevel.Information
     }));
+
+builder.Services.AddAutoMapper(typeof(ProdutoDTOMappingProfile));
 
 var app = builder.Build();
 
