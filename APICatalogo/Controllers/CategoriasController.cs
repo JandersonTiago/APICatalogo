99391a5d810951b2fb1773528a33baee95e77cef
Janderson Silva <janderson.tsilva@outlook.com>
2026-08-17 22:03:33 -0300

    Paginacao para Categorias - Implementacao

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -1,7 +1,9 @@
 ﻿using APICatalogo.DTOs;
 using APICatalogo.DTOs.Mappings;
+using APICatalogo.Pagination;
 using APICatalogo.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace APICatalogo.Controllers;
 
@@ -32,6 +34,28 @@ public class CategoriasController : ControllerBase
         var categoriasDTO = categorias.ToCategoriaDTOList();
 
         return Ok(categoriasDTO);
+    }
+
+    [HttpGet("Pagination")]
+    public ActionResult<IEnumerable<CategoriaDTO>> Get([FromQuery] CategoriasParameters categoriasParameters)
+    {
+        var categorias = _uof.CategoriaRepository.GetCategorias(categoriasParameters);
+
+        var metadata = new
+        {
+            categorias.TotalCount,
+            categorias.PageSize,
+            categorias.CurrentPage,
+            categorias.TotalPages,
+            categorias.HasNext,
+            categorias.HasPrevious
+        };
+
+        Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(metadata));
+
+        var categoriasDto = categorias.ToCategoriaDTOList();
+
+        return Ok(categoriasDto);
     }
 
     [HttpGet("{id:int}", Name = "ObterCategoria")]
--- APICatalogo/Repositories/CategoriaRepository.cs
+++ APICatalogo/Repositories/CategoriaRepository.cs
@@ -1,5 +1,6 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
+using APICatalogo.Pagination;
 
 namespace APICatalogo.Repositories;
 
@@ -7,5 +8,12 @@ public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
 {
     public CategoriaRepository(AppDbContext context) : base(context)
     {
+    }
+
+    public PagedList<Categoria> GetCategorias(CategoriasParameters categoriasParams)
+    {
+        var categorias = GetAll().OrderBy(c => c.CategoriaId).AsQueryable();
+        var categoriasOrdenadas = PagedList<Categoria>.ToPagedList(categorias, categoriasParams.PageNumber, categoriasParams.PageSize);
+        return categoriasOrdenadas;
     }
 }
