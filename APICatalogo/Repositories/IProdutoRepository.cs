1f330ca7d31e6d269bbb6cf3488e28a1165d6647
Janderson Silva <janderson.tsilva@outlook.com>
2026-08-18 21:47:47 -0300

    Implementando um Filtro para Produtos

--- APICatalogo/Repositories/IProdutoRepository.cs
+++ APICatalogo/Repositories/IProdutoRepository.cs
@@ -6,6 +6,7 @@ namespace APICatalogo.Repositories;
 public interface IProdutoRepository : IRepository<Produto>
 {
     //IEnumerable<Produto> GetProdutos(ProdutosParameters produtosParameters);
-    PagedList<Produto> GetProdutos(ProdutosParameters produtosParameters);
+    PagedList<Produto> GetProdutos(ProdutosParameters produtosParams);
+    PagedList<Produto> GetProdutosFiltroPreco(ProdutosFiltroPreco produtosFiltroParams);
     IEnumerable<Produto> GetProdutosPorCategoria(int id);
 }
